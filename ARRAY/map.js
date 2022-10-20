9fd6e1695511ca50e6179fbdb2675f40ec53b955
Titus Lagat <lagattyto@gmail.com>
2022-10-20 21:58:58 +0300

    mapped the array into a new array from calling a function in each array element.

---
+++ ARRAY/map.js
@@ -0,0 +1,4 @@
+//map() => returns a new array from calling a function from eery array element.
+const array = [2, 4, 6, 8, 10];
+const newArray = array.map(element => element * 2);
+console.log(newArray);// [4, 8, 12, 16, 20]
