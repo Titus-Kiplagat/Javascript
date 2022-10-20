c219b6c7839fcda04d8663df21c22a5ff13b747f
Titus Lagat <lagattyto@gmail.com>
2022-10-20 19:34:41 +0300

    Removed the first element in an array using shift method.
    Added elements at an array (at the beginning).

---
+++ ARRAY/shiftAndUnshift.js
@@ -0,0 +1,9 @@
+//shift()
+const fruits = ["Banana", "Orange", "Apple", "Mango"];
+fruits.shift();
+console.log(fruits);//["Orange", "Apple", "Mango"]
+
+//unshift()
+const Fruits = ["Banana", "Orange", "Apple", "Mango"];
+Fruits.unshift("addedLemon");
+console.log(Fruits);//["addedLemon", "Banana", "Orange", "Apple", "Mango"]
