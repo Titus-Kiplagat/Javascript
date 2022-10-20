fa829e9454bf98b4b39f6f5981541db66c3adf00
Titus Lagat <lagattyto@gmail.com>
2022-10-20 21:17:42 +0300

    filtered down elements from the array that passed the implemented test fuction.

---
+++ ARRAY/filter.js
@@ -0,0 +1,3 @@
+const words = ['spray', 'limit', 'elite', 'exuberant', 'destruction', 'present'];
+const longWords = words.filter(word => word.length > 6);
+console.log(longWords);//[ 'exuberant', 'destruction', 'present' ]
