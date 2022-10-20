fa0a4a21121e69c6df40d840ed3a7bc5d0b9bac7
Titus Lagat <lagattyto@gmail.com>
2022-10-20 19:16:47 +0300

    added new element to the end of the array using push method.
    removed the last element of an using pop method.

---
+++ ARRAY/popAndPush.js
@@ -0,0 +1,9 @@
+//Remove the last element in an array
+const fruits = ["Banana", "Orange", "Apple", "Mango"];
+fruits.pop();
+console.log(fruits); //["Banana", "Orange", "Apple"]
+
+//Add an element to the array (at the end)
+const Fruits = ["Banana", "Orange", "Apple", "Mango"];
+Fruits.push("addedMelon");
+console.log(Fruits)// ["Banana", "Orange", "Apple", "Mango", "addedMelon"]
